fa8f60c169a06b23e057cf8af8ec2540a7ed2044
fjp-pjf <feminjustin07@gmail.com>
2026-02-16 23:23:42 +0530

    signin in progress

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import pool from "./db";
 
 import hotelRoutes from "./routes/hotelRoutes";
 
--- frontend/app/(components)/login/page.tsx
+++ frontend/app/(components)/login/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { emailRegex, passwordRegex } from "@/app/utils/constants";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
 
 type Inputs = {
   email: string;
@@ -15,10 +17,21 @@ const Login = () => {
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data);
+    const isMatchEmail = emailRegex.test(data.email);
+    const isMatchPassword = passwordRegex.test(data.password);
+
+    if (!isMatchEmail || !isMatchPassword) {
+      return toast(
+        `Invalid Credentails. Please provide correct ${
+          !isMatchEmail ? "Email" : "Password"
+        }`
+      );
+    }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-purple-900 px-4">
+      <ToastContainer />
       <div className="w-full max-w-md bg-white rounded-2xl shadow-2xl p-8">
         <h2 className="text-3xl font-bold text-center text-purple-700 mb-8">
           Welcome Back
